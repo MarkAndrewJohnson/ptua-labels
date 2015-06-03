11d90acbfb0b901bdd388207ab9470820ae6e13b
Mark Johnson <mark@markjohnson.com.au>
2015-06-03 10:17:37 +0000

    Removed old code from script

--- labels.php
+++ labels.php
@@ -1,13 +1,6 @@
 <?php
 ini_set('memory_limit', '64M');
 if(isset($_FILES['current']) || isset($_FILES['expired'])) {
-	/*
-	 * $name= $_FILES["myfile"]["name"];
-	 * $type= $_FILES["myfile"]["type"];
-	 * $size= $_FILES["myfile"]["size"];
-	 * $temp= $_FILES["myfile"]["temp_name"];
-	 * $error= $_FILES["myfile"]["error"];
-	 */
 	$members = (object) array('current' => array(), 'expired' => array());
 	include_once('postcode_sort.inc.php');
 	$pcs = array();
@@ -64,10 +57,6 @@ if(isset($_FILES['current']) || isset($_FILES['expired'])) {
 				$member->label_expires = '';
 				$member->label_region = isset($pcs[$member->label_zip]) ? $pcs[$member->label_zip] : '';
 				$member->label_member_name = $member->full_name;
-				/*
-				$member->label_member_name = isset($member->first_name) && strlen($member->first_name) > 0 ?
-				$member->first_name . ' ' . $member->last_name : $member->employer;
-				 */
 			}
 		}
 	}
@@ -122,21 +111,13 @@ if(isset($_FILES['current']) || isset($_FILES['expired'])) {
 				}
 				$member->label_expires = 'EXPIRED';
 				$member->label_region = isset($pcs[$member->label_zip]) ? $pcs[$member->label_zip] : '';
-				$member->label_member_name = isset($member->first_name) && strlen($member->first_name) > 0 ?
-					$member->first_name . ' ' . $member->last_name : $member->employer;
+				$member->label_member_name = $member->full_name;
 			}
 		}
 	}
 
-	/* TODO remove */
-//	print_r($members);
-//	exit;
-
 	define('EXPIRED_MONTHS', 6);
 
-//var_dump($_SERVER);
-//var_dump($_SERVER['HTTP_USER_AGENT']);
-//exit;	
 	if(preg_match('/Mac OS X/', $_SERVER['HTTP_USER_AGENT']))
 	{
 		define('LEFT_MARGIN', 10);		
@@ -155,70 +136,6 @@ if(isset($_FILES['current']) || isset($_FILES['expired'])) {
 		define('CELL_HEIGHT', 35);
 	}
 
-	/* begin queries */
-	/*
-	// get indefinite types
-	$member_types_indef = array();
-	$sql = "select id from member_type where indefinite='Y'";
-	$db = db_connect();
-	$rs = $db->query($sql);
-	while($row = $rs->fetchRow()) $member_types_indef[] = $row['id'];
-	$member_types_indef = implode(', ', $member_types_indef);
-
-	$year = date('Y');
-	$month = date('m');
-
-	$month -= EXPIRED_MONTHS;
-	if($month < 1) 
-	{
-		$month += 12;
-		$year--;
-	}
-	if(strlen($month) == 1) $month = '0' . $month;
-	$from = $year . '-' . $month . '-01';
-
-	$year = date('Y');
-	$month = date('m');
-	$month += 3;
-	if($month > 12) 
-	{
-		$month -= 12;
-		$year++;
-	}
-	if(strlen($month) == 1) $month = '0' . $month;
-	$fromTo =  $year . '-' . $month . '-01';
-//		m.member_type_expires, datediff(now(), m.member_type_expires) as expired_days,
-	$sql = "select m.id, m.organisation, m.first_name, m.last_name, m.address_1, m.address2,
-		m.suburb, m.country, m.postcode, m.state,
-
-		m.member_type_expires, datediff('{$fromTo}', m.member_type_expires) as expired_days,
-		t.name, t.indefinite
-		from member m
-		inner join member_type t on t.id=m.member_type_id
-		where (m.member_type_id in ({$member_types_indef}) or m.member_type_expires >= '{$from}')
-		and m.flag='C'
-		and m.newsletter_paper='Y'
-		order by m.last_name, m.first_name";		
-	$rs = $db->query($sql);
-if($rs instanceof DB_Error) print_r($rs);
-	$members = array();
-	$members_expired = array();
-	$pcs = array();
-	include_once('scripts/include/postcode_sort.inc.php');
-	foreach($_pcsort as $_region => $_pcset) foreach ($_pcset as $_pc) $pcs["{$_pc}"] = $_region;
-	while($row = $rs->fetchRow())
-	{
-		$row['expires'] = ptua_format_expires_label($row['member_type_expires']);
-		$row['region'] = $pcs[$row['postcode']];
-		$row['member_name'] = isset($row['first_name']) && strlen($row['first_name']) > 0 ?
-			$row['first_name'] . ' ' . $row['last_name'] : $row['organisation'];
-		if($row['expired_days'] > 30) $members_expired[(-$row['expired_days']) . ' '  . $row['member_name']] = $row;
-		else $members[] = $row;
-	}
-	ksort($members_expired);
-	$db->disconnect();
-*/
-	/* end queries */
 /*
 	header("Cache-Control: ");
 	header("Pragma: ");
@@ -227,8 +144,6 @@ if($rs instanceof DB_Error) print_r($rs);
 	header("Content-Transfer-Encoding: binary\n");
 	header("Content-Length: " . filesize($full) . "\n");
 	*/
-	//send file contents
-
 
 	include_once('fpdf/fpdf.php');	
 	$pdf = new FPDF('P','mm','A4');
@@ -259,18 +174,12 @@ if($rs instanceof DB_Error) print_r($rs);
 			$pdf->Text(85, 3, "Page {$page}");
 		}
 
-//		$base_y = ($i * 34) + 20;
-//		$base_y = ($i * 35) + 14;
 		$base_y = ($i * CELL_HEIGHT) + TOP_OFFSET;
-//		$base_x = ($j * 64) + 6 + (($j - 1) * 5);
-//		$base_x = ($j * 64) + 10 + (($j - 1) * 5);
 		$base_x = ($j * CELL_WIDTH) + 11 + (($j - 1) * CELL_SPACING) + LEFT_OFFSET;
 		$pdf->SetFont('Arial','B',9);
 		if(isset($member->label_region) && strlen($member->label_region) > 0) {
-		//	$pdf->Text($base_x, $base_y, $member->label_region . ', Expires: ' . $member->label_expires);
 			$pdf->Text($base_x, $base_y, $member->label_region . ', ' . $member->label_expires);
 		} else {
-			//$pdf->Text($base_x, $base_y, 'Expires: ' . $member->label_expires);
 			$pdf->Text($base_x, $base_y, $member->label_expires);
 		}
 		$pdf->setXY($base_x, $base_y + 1);
@@ -291,7 +200,6 @@ if($rs instanceof DB_Error) print_r($rs);
 	} // foreach:member
 
 	$pdf->SetFont('Arial','',12);
-//	$pdf->SetFont('Arial','B',14);
 
 	$i = 0;
 	$j = 0;
@@ -311,36 +219,15 @@ if($rs instanceof DB_Error) print_r($rs);
 			$pdf->Text(85, 3, "Page {$page}");
 		}
 
-//		$base_y = ($i * 35) + 14;
-//		$base_x = ($j * 64) + 10 + (($j - 1) * 5);
-//		$base_y = ($i * 34) + 20;
-//		$base_y = ($i * 35) + 14;
-// PREVIOUS:
-//		$base_y = ($i * 35) + 16;
 		$base_y = ($i * CELL_HEIGHT) + TOP_OFFSET;
-//		$base_x = ($j * 64) + 6 + (($j - 1) * 5);
-//		$base_x = ($j * 64) + 10 + (($j - 1) * 5);
-// PREVIOUS:
-//		$base_x = ($j * 64) + 11 + (($j - 1) * 5);
 		$base_x = ($j * CELL_WIDTH) + 11 + (($j - 1) * CELL_SPACING) + LEFT_OFFSET;
 
 		$pdf->SetFont('Arial','B',9);
-		//if(strpos(strtolower(member->membership_names), 'indefinite') >= 0) $_exp = 'Indef';
 		if(strpos(strtolower($member->membership_names), 'indefinite') >= 0) {
 		   	$_exp = '';
 		} else {
 			$_exp = $member->label_expires;
 		}
-		/*
-		if(isset($member->label_region) && strlen($member->label_region) > 0)
-{
-			$pdf->Text($base_x, $base_y, $member->label_region . ', Exp:' . $_exp);
-}
-				else
-{
-					$pdf->Text($base_x, $base_y, 'Exp:' . $_exp);
-}
-		 */
 		if(isset($member->label_region) && strlen($member->label_region) > 0) {
 			$pdf->Text($base_x, $base_y, $member->label_region);
 		}
